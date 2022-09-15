9895c2025bf25d4a9ababa4f1d9972664704f8ed
TechWiz-3 <programmingplus1@gmail.com>
2022-09-15 22:57:31 +1000

    👌 IMPROVE: setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
+from pathlib import Path
+
+project_dir = Path(__file__).parent
+long_description = (project_dir / "README.md").read_text()
 
 setup(
     name="happyjar",
     version='1.0.0',
     description="Keep a happy jar from your terminal",
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     scripts=["happy"]
 )
